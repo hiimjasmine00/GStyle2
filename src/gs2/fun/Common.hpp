20766b5f40ea2eaf4b63ea2075d5502503b7dc2e
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-12-09 21:16:34 -0500

    Nope, nevermind

--- src/gs2/fun/Common.hpp
+++ src/gs2/fun/Common.hpp
@@ -90,7 +90,7 @@ namespace gs2::fun {
                 for (size_t ci=0; ci<children->count(); ++ci) {
                     auto c = typeinfo_cast<cocos2d::CCNode*>(children->objectAtIndex(ci));
                     try {
-                        auto desc = fmt::format("<CCNode | Tag: {}>", c->getTag());
+                        auto desc = std::string(c->description());
                         if (!token.empty() && desc.find(token) != std::string::npos) { next = c; break; }
                     } catch(...) { }
                 }
